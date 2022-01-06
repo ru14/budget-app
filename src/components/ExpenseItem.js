c05a41e540f8996be3be315dc9088439a4900f37
Raginee Upadhyaya <upadhyayaraginee@gmail.com>
2022-01-06 10:28:24 -0800

    data is not hard coded now

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css';
+
 function ExpenseItem() {
+    const expenseDate  = new Date(2022, 1, 4);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67
     return (
     <div className='expense-item'>
-        <div>Date</div>
+        <div>{expenseDate.toISOString}</div>
         <div className='expense-item__description'>
-            <h2>title</h2>
-            <div className='expense-item__price'>Amount</div>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>${expenseAmount}</div>
         </div>
     </div>
     );
