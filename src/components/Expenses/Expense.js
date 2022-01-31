c34a53ed8f83eb360e28bb8aa0191446298539f4
Raginee Upadhyaya <upadhyayaraginee@gmail.com>
2022-01-31 14:25:32 -0800

    data rendering and child to parent connection

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,16 +1,37 @@
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expense.css';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expense = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2022');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
 
     return (
-        <Card className='expenses'>
-            {props.items.map((item) => (
-                <ExpenseItem title={item.title} amount={item.amount} date={item.date}>
-                </ExpenseItem>
-            ))}
-        </Card>
+        <div>
+            <Card className='expenses'>
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}
+                />
+                {filteredExpenses.map((item) => (
+                    <ExpenseItem
+                        key={item.id}
+                        title={item.title}
+                        amount={item.amount}
+                        date={item.date}>
+                    </ExpenseItem>
+                ))}
+            </Card>
+        </div>
     )
 }
 
