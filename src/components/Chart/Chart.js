f448ca516c2712fc67a38937f56e7ca1b4fa292f
Raginee Upadhyaya <upadhyayaraginee@gmail.com>
2022-02-11 08:43:04 -0800

    error fixed

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,7 +4,9 @@ import './Chart.css';
 
 const Chart = (props) => {
   const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+   // for every datapoint we just return datapoint value
   const totalMaximum = Math.max(...dataPointValues);
+      //since max want argument and dataPointValue in still and array we use spread operator to pull out value as argument
   return (
     <div className='chart'>
       {props.dataPoints.map((dataPoint) => (
--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -19,7 +19,8 @@ const ExpensesChart = (props) => {
 
   for (const expense of props.expenses) {
     const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
-    chartDataPoints[expenseMonth].value += expense.amount;
+    chartDataPoints[expenseMonth].value += expense.amount;//reach out to chartDataPoint for givrn month and update value by addeding expense amount to it by  += operator 
+
   }
 
   return <Chart dataPoints={chartDataPoints} />;
