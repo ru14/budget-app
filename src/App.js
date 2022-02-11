7c5862427fa611020b96dd0779c853ec4c040ba5
Raginee Upadhyaya <upadhyayaraginee@gmail.com>
2022-02-11 08:58:50 -0800

    error fixed

--- src/App.js
+++ src/App.js
@@ -28,6 +28,12 @@ const DUMMY_EXPENSES = [
     amount: 450,
     date: new Date(2021, 5, 12),
   },
+  {
+    id: 'e5',
+    title: 'Bose HeadPhones',
+    amount: 350,
+    date: new Date(2022, 2, 12),
+  },
 
 ];
 
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,9 +4,9 @@ import './Chart.css';
 
 const Chart = (props) => {
   const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
-   // for every datapoint we just return datapoint value
+  // for every datapoint we just return datapoint value
   const totalMaximum = Math.max(...dataPointValues);
-      //since max want argument and dataPointValue in still and array we use spread operator to pull out value as argument
+  //since max want argument and dataPointValue in still and array we use spread operator to pull out value as argument
   return (
     <div className='chart'>
       {props.dataPoints.map((dataPoint) => (
